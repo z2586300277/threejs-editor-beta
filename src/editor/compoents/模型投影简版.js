b8c5214fc5a2e64f677e07c0041a10598475482b
zhangjingguang <2586300277@qq.com>
2026-06-04 19:25:27 +0800

    投影

---
+++ src/editor/compoents/模型投影简版.js
@@ -0,0 +1,40 @@
+import * as THREE from 'three';
+import { mergeGeometries } from 'three/examples/jsm/utils/BufferGeometryUtils.js';
+import { ProjectionGenerator } from '@antoninrousset/three-edge-projection';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+export default {
+    name: '模型投影',
+    label: '模型投影',
+    async create(storage, { scene }) {
+        const gltf = await new GLTFLoader().loadAsync('https://z2586300277.github.io/three-editor/dist/files/resource/datacenter.glb');
+        const model = gltf.scene;
+        const group = new THREE.Group();
+        const geometries = [];
+
+        model.updateMatrixWorld(true);
+        model.traverse((child) => {
+            if (!child.isMesh) return;
+
+            const geometry = child.geometry.index ? child.geometry.toNonIndexed() : child.geometry.clone();
+            geometry.applyMatrix4(child.matrixWorld);
+
+            const mergedPart = new THREE.BufferGeometry();
+            mergedPart.setAttribute('position', geometry.getAttribute('position').clone());
+            geometries.push(mergedPart);
+        });
+
+        const mergedGeometry = mergeGeometries(geometries);
+        const projectionGeometry = await new ProjectionGenerator().generateAsync(mergedGeometry);
+        const lines = new THREE.LineSegments(
+            projectionGeometry,
+            new THREE.LineBasicMaterial({ color: 0x00000 })
+        );
+
+        lines.position.y = 0.01;
+        // group.add(model);
+        group.add(lines);
+
+        return group;
+    }
+};
